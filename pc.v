fa0b40ed387cbbb6a76aeb01a92de2539de88a41
Arjun Londhe <amlondhe8@gmail.com>
2026-08-06 07:36:42 +0530

    Delete pc.v

--- pc.v
+++
@@ -1,9 +0,0 @@
-module program_counter(clk,rst,pc_next,pc);
-input clk,rst;
-input wire [31:0]pc_next;
-output reg [31:0]pc;
-always @(posedge clk) begin
-    if (rst) pc=32'h00000000;
-    else pc<=pc_next;
-end
-endmodule
