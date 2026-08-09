df9887c08181705d0fa6bab2e0ff40949e0b77de
Arjun Londhe <amlondhe8@gmail.com>
2026-08-09 11:28:46 +0530

    Delete RTL directory

--- RTL/imem.v
+++
@@ -1,7 +0,0 @@
-module imem #(parameter depth = 256)(addr,instr);
-input [31:0]addr;
-output [31:0] instr;
-reg [31:0] mem[0:depth-1];
-initial $readmemh("program.hex",mem);
-assign instr=mem[addr[31:2]];
-endmodule
