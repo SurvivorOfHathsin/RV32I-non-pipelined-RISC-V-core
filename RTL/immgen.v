d9355a02232c99099457b50d81de48da22d8cb9a
Arjun Londhe <amlondhe8@gmail.com>
2026-08-09 11:32:20 +0530

    CU and IMMGEN
    created CU and syntax error in IMMGEN

--- RTL/immgen.v
+++ RTL/immgen.v
@@ -9,8 +9,8 @@ always @(*) begin
        3'b000: immediate= {{20{instr[31]}},instr[31:20]};
        3'b001: immediate= {{20{instr[31]}},instr[31:25],instr[11:7]};
        3'b010: immediate= {{19{instr[31]}}, instr[31], instr[7],instr[30:25], instr[11:8], 1'b0};
-       3'b011: imm = {instr[31:12], 12'b0};
-       3'b100: imm = {{11{instr[31]}}, instr[31], instr[19:12],instr[20], instr[30:21], 1'b0};
+       3'b011: immediate = {instr[31:12], 12'b0};
+       3'b100: immediate = {{11{instr[31]}}, instr[31], instr[19:12],instr[20], instr[30:21], 1'b0};
     endcase
 end
 endmodule
